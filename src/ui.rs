3e55efc22cc2c439f9f779ee537dcd922636f174
Ivan Schuetz <ivanschuetz@gmail.com>
2024-10-19 12:05:26 +0200

    Show keyboard shortcuts

--- src/ui.rs
+++ src/ui.rs
@@ -54,7 +54,7 @@ pub fn add_ui(app: &mut App) {
                 rot_z_button_handler,
             ),
         )
-        .add_systems(Startup, setup_ui);
+        .add_systems(Startup, (setup_ui, setup_info_labels));
 }
 
 /// adds right column with ui elements to scene
@@ -555,5 +555,54 @@ pub fn rot_z_button_handler(
                 );
             }
         }
+    }
+}
+
+pub fn setup_info_labels(commands: Commands, asset_server: Res<AssetServer>) {
+    let font = asset_server.load("fonts/FiraMono-Medium.ttf");
+    add_info_labels(commands, &font);
+}
+
+fn add_info_labels(mut commands: Commands, font: &Handle<Font>) {
+    commands.spawn(generate_info_label(font, "move right: a", 0.0));
+    commands.spawn(generate_info_label(font, "move left: d", 20.0));
+    commands.spawn(generate_info_label(font, "zoom in: w", 40.0));
+    commands.spawn(generate_info_label(font, "zoom out: s", 60.0));
+    commands.spawn(generate_info_label(
+        font,
+        "rotate around z: i / shift-i",
+        80.0,
+    ));
+    commands.spawn(generate_info_label(
+        font,
+        "rotate around y: o / shift-o",
+        100.0,
+    ));
+    commands.spawn(generate_info_label(
+        font,
+        "rotate around x: p / shift-p",
+        120.0,
+    ));
+}
+
+fn generate_info_label(font: &Handle<Font>, label: &str, top: f32) -> TextBundle {
+    TextBundle {
+        style: Style {
+            position_type: PositionType::Relative,
+            top: Val::Px(top),
+            left: Val::Px(10.0),
+            width: Val::Auto,
+            height: Val::Auto,
+            ..default()
+        },
+        text: Text::from_section(
+            label.to_string(),
+            TextStyle {
+                font: font.clone(),
+                font_size: 14.0,
+                color: Color::WHITE,
+            },
+        ),
+        ..default()
     }
 }
