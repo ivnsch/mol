a5fa68b3ef1e2c6460cbcf91f836a6cceeb85b37
Ivan Schuetz <ivanschuetz@gmail.com>
2024-10-20 17:54:25 +0200

    Set log level to info
    debug level (default) too many messages, maybe caused issues

--- src/init_wasm_log.rs
+++ src/init_wasm_log.rs
@@ -1,9 +1,9 @@
-use log::info;
+use log::{info, Level};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn init_log() -> Result<(), JsValue> {
-    wasm_logger::init(wasm_logger::Config::default());
+    wasm_logger::init(wasm_logger::Config::new(Level::Info));
     info!("Initialized wasm logs");
     Ok(())
 }
