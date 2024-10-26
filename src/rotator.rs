540eaee9a5e214c8269b75c7a6306dee28fa0534
Ivan Schuetz <ivanschuetz@gmail.com>
2024-10-26 12:14:08 +0200

    Refactor rotator

--- src/rotator.rs
+++ src/rotator.rs
@@ -64,34 +64,16 @@ fn run_molecule_rotator(
     mut sphere: Query<&mut Transform, With<MyMolecule>>,
     mut camera: Query<&mut Rotator>,
 ) {
-    if let Ok(mut sphere_transform) = sphere.get_single_mut() {
-        let camera = camera.get_single_mut();
-        if let Ok(rotator) = camera {
-            let mut rotation = 0.03;
-            if key_input.pressed(rotator.key_shift_left)
-                || key_input.pressed(rotator.key_shift_right)
-            {
-                rotation = -rotation;
-            }
-
-            if key_input.pressed(rotator.key_y) {
-                sphere_transform.rotate_around(
-                    Vec3::ZERO,
-                    Quat::from_euler(EulerRot::XYZ, 0.0, rotation, 0.0),
-                );
-            }
-            if key_input.pressed(rotator.key_z) {
-                sphere_transform.rotate_around(
-                    Vec3::ZERO,
-                    Quat::from_euler(EulerRot::XYZ, 0.0, 0.0, rotation),
-                );
-            }
-            if key_input.pressed(rotator.key_x) {
-                sphere_transform.rotate_around(
-                    Vec3::ZERO,
-                    Quat::from_euler(EulerRot::XYZ, rotation, 0.0, 0.0),
-                );
-            }
+    if let Ok(mut transform) = sphere.get_single_mut() {
+        if let Ok(rotator) = camera.get_single_mut() {
+            rotate(
+                key_input,
+                &rotator,
+                &mut transform,
+                rotator.key_x,
+                rotator.key_y,
+                rotator.key_z,
+            );
         }
     };
 }
@@ -101,32 +83,47 @@ fn run_camera_rotator(
     key_input: Res<ButtonInput<KeyCode>>,
     mut camera: Query<(&mut Transform, &mut Rotator), With<Camera>>,
 ) {
-    if let Ok((mut transform, mut controller)) = camera.get_single_mut() {
-        let mut rotation = 0.03;
+    if let Ok((mut transform, mut rotator)) = camera.get_single_mut() {
+        rotate(
+            key_input,
+            &rotator,
+            &mut transform,
+            rotator.key_x,
+            rotator.key_y,
+            rotator.key_z,
+        );
+    }
+}
 
-        if key_input.pressed(controller.key_shift_left)
-            || key_input.pressed(controller.key_shift_right)
-        {
-            rotation = -rotation;
-        }
+fn rotate(
+    key_input: Res<ButtonInput<KeyCode>>,
+    rotator: &Rotator,
+    transform: &mut Transform,
+    x_key: KeyCode,
+    y_key: KeyCode,
+    z_key: KeyCode,
+) {
+    let mut rotation = 0.03;
+    if key_input.pressed(rotator.key_shift_left) || key_input.pressed(rotator.key_shift_right) {
+        rotation = -rotation;
+    }
 
-        if key_input.pressed(controller.key_i) {
-            transform.rotate_around(
-                Vec3::ZERO,
-                Quat::from_euler(EulerRot::XYZ, 0.0, rotation, 0.0),
-            );
-        }
-        if key_input.pressed(controller.key_o) {
-            transform.rotate_around(
-                Vec3::ZERO,
-                Quat::from_euler(EulerRot::XYZ, 0.0, 0.0, rotation),
-            );
-        }
-        if key_input.pressed(controller.key_p) {
-            transform.rotate_around(
-                Vec3::ZERO,
-                Quat::from_euler(EulerRot::XYZ, rotation, 0.0, 0.0),
-            );
-        }
+    if key_input.pressed(y_key) {
+        transform.rotate_around(
+            Vec3::ZERO,
+            Quat::from_euler(EulerRot::XYZ, 0.0, rotation, 0.0),
+        );
+    }
+    if key_input.pressed(z_key) {
+        transform.rotate_around(
+            Vec3::ZERO,
+            Quat::from_euler(EulerRot::XYZ, 0.0, 0.0, rotation),
+        );
+    }
+    if key_input.pressed(x_key) {
+        transform.rotate_around(
+            Vec3::ZERO,
+            Quat::from_euler(EulerRot::XYZ, rotation, 0.0, 0.0),
+        );
     }
 }
